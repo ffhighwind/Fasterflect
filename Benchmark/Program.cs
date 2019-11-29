633e588e1a16959fd371220ee9b66b4458aaaad2
Wesley Hamilton <ffhighwind@gmail.com>
2019-11-29 10:13:19 -0700

    Added README and license to Benchmark project.

--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -1,4 +1,29 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿#region License
+
+// The MIT License
+// 
+// Copyright (c) 2016 Matt Warren
+// Copyright (c) 2019 Wesley Hamilton
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+// and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+// of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
+// 
+// The latest version of this file can be found at https://github.com/ffhighwind/fasterflect
+
+#endregion
+
+using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 using System;
@@ -19,9 +44,7 @@ namespace Benchmark
 		static void Main(string[] args)
 		{
 			var summary = BenchmarkRunner.Run<Getters>();
-			//Console.ReadLine();
 			var summary2 = BenchmarkRunner.Run<Setters>();
-			//Console.ReadLine();
 		}
 	}
 }
